b29d63a4eb86289162d4088f1c854002c80226bf
P.A.J.U.Pallewela <janithup10@gmail.com>
2025-09-22 16:48:49 +0530

    create lecturer table

--- university-janith.sql
+++ university-janith.sql
@@ -1,6 +1,8 @@
-mysql> CREATE DATABASE university;
+CREATE DATABASE university;
 
- CREATE TABLE Student (  
+USE university;
+
+CREATE TABLE Student (  
     RegNo VARCHAR(15) 
     PRIMARY KEY,   
     Name VARCHAR(100),   
@@ -9,4 +11,17 @@ mysql> CREATE DATABASE university;
     Status ENUM('Proper','Repeat','Suspended'),  
     Contact VARCHAR(15),   
     Email VARCHAR(100) 
+);
+
+CREATE TABLE Lecturer (
+    LecturerID INT AUTO_INCREMENT PRIMARY KEY,
+    StaffCode VARCHAR(10) UNIQUE NOT NULL,
+    FirstName VARCHAR(50) NOT NULL,
+    LastName VARCHAR(50) NOT NULL,
+    Email VARCHAR(100) UNIQUE NOT NULL,
+    Phone VARCHAR(15),
+    DepartmentID INT NOT NULL,
+    UserID INT NOT NULL, -- FK to Users table
+    Role ENUM('Lecturer', 'Dean') DEFAULT 'Lecturer',
+    Status ENUM('Active', 'Inactive') DEFAULT 'Active'
 );
