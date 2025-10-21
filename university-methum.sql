8119653e51f708a354b3045505e1554402199c40
shanuka-saranga <shanukasaranga974@gmail.com>
2025-10-21 22:38:21 +0530

    Remove user creation and privilege grants for Admin, Dean, Lecturer, Technical Officer, and Student from SQL script

--- university-methum.sql
+++ university-methum.sql
@@ -150,22 +150,3 @@ CREATE TABLE Results (
 
 -- MYSQL USER ACCOUNTS
 
-CREATE USER 'Admin'@'%' IDENTIFIED BY 'Admin@123';
-GRANT ALL PRIVILEGES ON university.* TO 'Admin'@'%' WITH GRANT OPTION;
-
-CREATE USER 'Dean'@'%' IDENTIFIED BY 'Dean@123';
-GRANT ALL PRIVILEGES ON university.* TO 'Dean'@'%';
-
-CREATE USER 'Lecturer'@'%' IDENTIFIED BY 'Lecturer@123';
-GRANT ALL PRIVILEGES ON university.* TO 'Lecturer'@'%';
-REVOKE CREATE USER ON *.* FROM 'Lecturer'@'%';
-
-CREATE USER 'TechnicalOfficer'@'%' IDENTIFIED BY 'Tech@123';
-GRANT SELECT, INSERT, UPDATE ON university.Attendance TO 'TechnicalOfficer'@'%';
-
-CREATE USER 'Student'@'%' IDENTIFIED BY 'Student@123';
-
-GRANT SELECT ON university.final_attendance_view TO 'Student'@'%';
-GRANT SELECT ON university.final_grades_view TO 'Student'@'%';
-
-FLUSH PRIVILEGES;
